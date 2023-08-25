f54b7c3e3a155b5629e9008ed979e3b903723f5b
Amoda-Nilnuwan <amoda1998@gmail.com>
2023-08-25 19:41:14 +0530

    w2

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
             alert(result);
           }
 
-          const ws = new WebSocket('wss://' + window.location.host + ':10000');
+          const ws = new WebSocket('wss://bulk-url-tester:10000');
 
           ws.addEventListener('open', (event) => {
             console.log('WebSocket connection opened.');
